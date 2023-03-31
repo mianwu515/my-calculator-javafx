223750fb7315dcfce91692b3a9eb64597f477185
mw515 <mw515@duke.edu>
2023-03-31 07:28:38 -0400

    Button.setMaxWidth(Height); Column(Row)Constraints

--- app/src/main/java/edu/duke/ece651/calculator/App.java
+++ app/src/main/java/edu/duke/ece651/calculator/App.java
@@ -3,7 +3,9 @@ package edu.duke.ece651.calculator;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -20,6 +22,17 @@ public class App extends Application {
 
     // Scene scene = new Scene(root, 640, 480);
     GridPane gp = new GridPane();
+    for (int i = 0; i < 4; i++) {
+      ColumnConstraints cc = new ColumnConstraints();
+      cc.setPercentWidth(25);
+      gp.getColumnConstraints().add(cc);
+    }
+    for (int i = 0; i < 5; i++) {
+      RowConstraints rc = new RowConstraints();
+      rc.setPercentHeight(20);
+      gp.getRowConstraints().add(rc);
+    }
+
     // gp.add(new Button("+"), 0, 0);
     // gp.add(new Button("-"), 1, 0);
     // gp.add(new Button("*"), 2, 0);
@@ -39,12 +52,24 @@ public class App extends Application {
 
     int rows[] = new int[] { 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4 };
     int cols[] = new int[] { 0, 1, 2, 3, 0, 1, 2, 0, 1, 2, 0, 1, 2, 2 };
+
     for (int i = 0; i < labels.length; i++) {
       Button b = new Button(labels[i]);
       gp.add(b, cols[i], rows[i]);
+      b.setMaxWidth(Double.MAX_VALUE);
+      b.setMaxHeight(Double.MAX_VALUE);
     }
-    gp.add(new Button("0"), 0, 4, 2, 1);
-    gp.add(new Button("E\nn\nt\ne\nr"), 3, 1, 1, 3);
+
+    Button b = new Button("0");
+    b.setMaxWidth(Double.MAX_VALUE);
+    b.setMaxHeight(Double.MAX_VALUE);
+    gp.add(b, 0, 4, 2, 1);
+
+    b = new Button("E\nn\nt\ne\nr");
+    b.setMaxWidth(Double.MAX_VALUE); // Note this change
+    b.setMaxHeight(Double.MAX_VALUE); // Note this change
+    gp.add(b, 3, 1, 1, 3);
+
     Scene scene = new Scene(gp, 640, 480);
     // Scene scene = new Scene(new StackPane(l, circ), 640, 480);
     stage.setTitle("My Calculator Application");
