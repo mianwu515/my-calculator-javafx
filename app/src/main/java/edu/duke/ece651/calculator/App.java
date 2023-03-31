9154e4a076e5b2747436628317c358d312fce694
mw515 <mw515@duke.edu>
2023-03-30 23:28:56 -0400

    add GridPane + Buttons

--- app/src/main/java/edu/duke/ece651/calculator/App.java
+++ app/src/main/java/edu/duke/ece651/calculator/App.java
@@ -1,11 +1,9 @@
 package edu.duke.ece651.calculator;
 
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.scene.shape.Circle;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -14,14 +12,41 @@ public class App extends Application {
   public void start(Stage stage) {
     String javaVersion = System.getProperty("java.version");
     String javafxVersion = System.getProperty("javafx.version");
-    Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+    // Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " +
+    // javaVersion + ".");
 
-    Circle circ = new Circle(40, 40, 30);
-    //Group root = new Group(circ, l);
+    // Circle circ = new Circle(40, 40, 30);
+    // Group root = new Group(circ, l);
 
-    //Scene scene = new Scene(root, 640, 480);
+    // Scene scene = new Scene(root, 640, 480);
+    GridPane gp = new GridPane();
+    // gp.add(new Button("+"), 0, 0);
+    // gp.add(new Button("-"), 1, 0);
+    // gp.add(new Button("*"), 2, 0);
+    // gp.add(new Button("/"), 3, 0);
 
-    Scene scene = new Scene(new StackPane(l, circ), 640, 480);
+    // gp.add(new Button("7"), 0, 1);
+    // gp.add(new Button("8"), 1, 1);
+    // gp.add(new Button("9"), 2, 1);
+    // gp.add(new Button("4"), 0, 2);
+    // gp.add(new Button("5"), 1, 2);
+    // gp.add(new Button("6"), 2, 2);
+    // gp.add(new Button("1"), 0, 3);
+    // gp.add(new Button("2"), 1, 3);
+    // gp.add(new Button("3"), 2, 3);
+
+    String[] labels = new String[] { "+", "-", "*", "/", "7", "8", "9", "4", "5", "6", "1", "2", "3", "." };
+
+    int rows[] = new int[] { 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4 };
+    int cols[] = new int[] { 0, 1, 2, 3, 0, 1, 2, 0, 1, 2, 0, 1, 2, 2 };
+    for (int i = 0; i < labels.length; i++) {
+      Button b = new Button(labels[i]);
+      gp.add(b, cols[i], rows[i]);
+    }
+    gp.add(new Button("0"), 0, 4, 2, 1);
+    gp.add(new Button("E\nn\nt\ne\nr"), 3, 1, 1, 3);
+    Scene scene = new Scene(gp, 640, 480);
+    // Scene scene = new Scene(new StackPane(l, circ), 640, 480);
     stage.setTitle("My Calculator Application");
     stage.setScene(scene);
     stage.show();
